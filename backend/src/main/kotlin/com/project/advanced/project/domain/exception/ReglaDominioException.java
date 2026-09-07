44cf2ee67489e85dbefcf3dc4df064d284b6b5f7
boterop <boterop22@gmail.com>
2026-09-07 11:53:35 -0500

    Translate java code to kotlin

--- backend/src/main/kotlin/com/project/advanced/project/domain/exception/ReglaDominioException.java
+++
@@ -1,7 +0,0 @@
-package com.project.advanced.project.domain.exception
-
-public class ReglaDominioException extends RuntimeException {
-  public ReglaDominioException(String mensaje) {
-    super(mensaje);
-  }
-}
---
+++ backend/src/main/kotlin/com/project/advanced/project/domain/exception/ReglaDominioException.kt
@@ -0,0 +1,5 @@
+package com.project.advanced.project.domain.exception
+
+class ReglaDominioException(
+    mensaje: String,
+) : RuntimeException(mensaje)
